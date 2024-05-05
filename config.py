8ad7bc83c1e1f21fcc1b1e02341e6186b8cc4eda
Thomas Southcott <tom.e.southcott@gmail.com>
2024-05-05 18:37:03 +0100

    Config for Fitbit API Config in lower case now

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 import configparser
 import dataclasses
 import logging
+from secrets import token_hex
 
 from constants import InfoTypes, BannerTypes, VerticalAlignment, HorizontalAlignment
 from image_helper import check_image_path
@@ -12,6 +13,14 @@ def get_config_item(config, group, key):
     """Returns the value of the key in the group"""
     try:
         return config[group][key]
+    except KeyError as exc:
+        raise KeyError(f"Key {key} not found in group {group}") from exc
+    
+def set_config_item(config, group, key, value):
+    """Sets the value of the key in the group"""
+    try:
+        config[group][key] = value
+        config.write(open("config.ini", "w"))
     except KeyError as exc:
         raise KeyError(f"Key {key} not found in group {group}") from exc
 
@@ -74,18 +83,92 @@ class WeatherConfig:
         except Exception as exc:
             raise FileNotFoundError("Config file not found") from exc
         self.api_key = get_config_item(config,"WEATHER","API_KEY")
-        self.api_urls = get_config_item(config,"WEATHER","API_URL").split(",")
+        self.api_urls = get_config_item(config,"WEATHER","API_URL").split(" ")
         self.api_refresh_interval = int(get_config_item(config,"WEATHER","REFRESH_INTERVAL"))
         self.city = get_config_item(config,"WEATHER","CITY")
         self.units = get_config_item(config,"WEATHER","UNITS")
 
-_config = Config()
-_weather_config = WeatherConfig()
+@dataclasses.dataclass
+class TextBoxConfig:
+    """Class to hold the text box configuration"""
+    def __init__(self):
+        try:
+            config = configparser.ConfigParser()
+            config.read("config.ini")
+        except Exception as exc:
+            raise FileNotFoundError("Config file not found") from exc
+        self.text = get_config_item(config,"TEXTBOX","TEXT")
+
+@dataclasses.dataclass
+class FitbitConfig:
+    """Class to hold the fitbit configuration"""
+    def __init__(self):
+        try:
+            config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+            config.read("config.ini")
+        except Exception as exc:
+            raise FileNotFoundError("Config file not found") from exc
+        self.api_client_id = get_config_item(config,"FITBIT","API_CLIENT_ID")
+        self.api_secret = get_config_item(config,"FITBIT","API_SECRET")
+        self.api_urls = get_config_item(config,"FITBIT","API_URL").split(" ")
+        self.api_refresh_interval = int(get_config_item(config,"FITBIT","REFRESH_INTERVAL"))
+        self.api_key = get_config_item(config,"FITBIT","API_KEY")
+        self.api_access_token = get_config_item(config,"FITBIT","API_ACCESS_TOKEN")
+        self.api_refresh_token = get_config_item(config,"FITBIT","API_REFRESH_TOKEN")
+        expiry = get_config_item(config,"FITBIT","API_EXPIRY")
+        if expiry == "":
+            self.api_expiry = 0
+        else:
+            self.api_expiry = float(get_config_item(config,"FITBIT","API_EXPIRY"))
+        self.user_id = get_config_item(config,"FITBIT","USER_ID")
+        self.units = get_config_item(config,"FITBIT","UNITS")
+        self.scope = get_config_item(config,"FITBIT","API_SCOPE")
+        try:
+            self.code_verifier = get_config_item(config,"FITBIT","CODE_VERIFIER")
+            if self.code_verifier == "":
+                logger.warning("[CONFIG] No code verifier found in config.ini")
+                self.code_verifier = token_hex(32)
+                set_config_item(config,"FITBIT","CODE_VERIFIER",self.code_verifier)
+        except KeyError:
+            logger.warning("[CONFIG] No code verifier found in config.ini")
+            self.code_verifier = token_hex(32)
+            set_config_item(config,"FITBIT","CODE_VERIFIER",self.code_verifier)
+    
+    def set_tokens(self, access_token, refresh_token, expiry, user_id):
+        """Sets the access token and refresh token"""
+        try:
+            config = configparser.ConfigParser()
+            config.read("config.ini")
+        except Exception as exc:
+            raise FileNotFoundError("Config file not found") from exc
+        self.api_access_token = access_token
+        self.api_refresh_token = refresh_token
+        self.api_expiry = expiry
+        self.user_id = user_id
+        if expiry is not str:
+            expiry = str(expiry)
+        set_config_item(config,"FITBIT","API_ACCESS_TOKEN",access_token)
+        set_config_item(config,"FITBIT","API_REFRESH_TOKEN",refresh_token)
+        set_config_item(config,"FITBIT","API_EXPIRY",expiry)
+        set_config_item(config,"FITBIT","USER_ID",user_id)
+
+_CONFIG = Config()
+_TEXTBOX_CONFIG = TextBoxConfig()
+_WEATHER_CONFIG = WeatherConfig()
+_FITBIT_CONFIG = FitbitConfig()
 
 def get_config():
     """Returns the currently loaded configuration"""
-    return _config
+    return _CONFIG
+
+def get_textbox_config():
+    """Returns the currently loaded textbox configuration"""
+    return _TEXTBOX_CONFIG
 
 def get_weather_config():
     """Returns the currently loaded weather configuration"""
-    return _weather_config
+    return _WEATHER_CONFIG
+
+def get_fitbit_config():
+    """Returns the currently loaded fitbit configuration"""
+    return _FITBIT_CONFIG
